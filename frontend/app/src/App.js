dd4264498e3c4be7ba890a89c272c9b4f7b97cd3
Nolan2IT <tuitenol@berkeleyprep.org>
2022-07-23 05:58:13 -0500

    characters + buttons to swap

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,19 +1,37 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import spotify_logo from "./images/spotify_logo.png";
+import spongebob from "./characters/spongebob.jpeg";
+import patrick from "./characters/patrick.png";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function App() {
+  const character = [patrick, spongebob];
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <div className="Header-text"> Silver Dollop </div>
       </header>
       <body>
-        <img src={spotify_logo} className="App-logo" alt="logo" />
+        <div className="Chracter-content">
+          <button className="Arrow_button" onClick={() => setCount(count - 1)}>
+            <ArrowBackIosIcon />
+          </button>
+          <img
+            src={character[Math.abs(count % character.length)]}
+            className="App-logo"
+            alt="logo"
+          />
+          <button className="Arrow_button" onClick={() => setCount(count + 1)}>
+            <ArrowForwardIosIcon />
+          </button>
+        </div>
         <div className="App-body">
-          <button className="Spotify_button">Login with Spotify</button>
-          <button className="Guest_button">Play as Guest</button>
+          <button className="Play_button">Play</button>
+          <button className="Host_button">Host</button>
         </div>
       </body>
     </div>
