74849e8d8c7fcd081add384962904d736153ede3
Nolan2IT <tuitenol@berkeleyprep.org>
2022-07-23 07:53:17 -0500

    nickname input + interactive host button

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import spotify_logo from "./images/spotify_logo.png";
 import spongebob from "./characters/spongebob.jpeg";
 import patrick from "./characters/patrick.png";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import TextField from "@mui/material/TextField";
 
 function App() {
   const character = [patrick, spongebob];
+  const buttons = [
+    "Spotify_button",
+    "Guest_button",
+    "Spotify_button_hidden",
+    "Guest_button_hidden",
+  ];
   const [count, setCount] = useState(0);
+  const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -29,9 +35,30 @@ function App() {
             <ArrowForwardIosIcon />
           </button>
         </div>
+        <div className="Text-box">
+          <form>
+            <label>
+              <input
+                className="Text-input"
+                type="text"
+                name="Nickname"
+                placeholder="Nickname"
+                color="white"
+              />
+            </label>
+          </form>
+        </div>
         <div className="App-body">
           <button className="Play_button">Play</button>
-          <button className="Host_button">Host</button>
+          <button className="Host_button" onClick={() => setNum(num + 1)}>
+            Host
+          </button>
+          <button className={buttons[(2 * num) % buttons.length]}>
+            Log in with Spotify
+          </button>
+          <button className={buttons[(1 + 2 * num) % buttons.length]}>
+            Continue as Guest
+          </button>
         </div>
       </body>
     </div>
